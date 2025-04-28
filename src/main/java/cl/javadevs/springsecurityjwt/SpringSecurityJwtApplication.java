ae6149b62822f66993835f6fb5053fda62d68bf8
ariansevillano <ariansevillano1@gmail.com>
2025-04-28 12:14:59 -0500

    Se agrega CORS

--- src/main/java/cl/javadevs/springsecurityjwt/SpringSecurityJwtApplication.java
+++ src/main/java/cl/javadevs/springsecurityjwt/SpringSecurityJwtApplication.java
@@ -13,4 +13,18 @@ public class SpringSecurityJwtApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityJwtApplication.class, args);
     }
+
+    @Configuration
+    public static class Myconfiguration{
+        @Bean
+        public WebMvcConfigurer corsConfigurer(){
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+                }
+            };
+        }
+    }
 }
